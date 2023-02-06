43623a82af341f9b4d2f7a8e38e78141e5ba4eb9
Joon Park <sargadil@gmail.com>
2023-02-06 11:51:00 +0900

    230206-4

--- src/Champ.java
+++ src/Champ.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; //스캐너 클래스를 사용할 것이므로 스캐너 클래스 호출
 
-public class Champ{ //챔피언 인스턴스
+public class Champ{ //챔피언 클래스
 	String Q = "";
 	String W = "";
 	String E = "";
@@ -16,12 +16,12 @@ public class Champ{ //챔피언 인스턴스
 	}
 	
 	public static void main(String[] args) {
-		Champ Garen = new Champ("결정타", "용기", "심판", "데마시아의 정의", "가렌");
+		Champ Garen = new Champ("결정타", "용기", "심판", "데마시아의 정의", "가렌"); // Champ 클래스를 이용해 Garen 오브젝트 생성
 		Champ Amumu = new Champ("붕대 던지기", "절망", "짜증내기", "슬픈 미라의 저주", "아무무");
 		Champ Kayle = new Champ("광휘의 일격", "천상의 축복", "화염주문검", "신성한 심판", "케일");
 		
 		System.out.println("알고 싶은 챔피언을 번호로 선택하세요. \n 1. 가렌 \n 2. 아무무 \n 3. 케일 ");
-		Scanner scn = new Scanner(System.in); // 값을 입력받기 위해 Scanner 객체 생성
+		Scanner scn = new Scanner(System.in); // 값을 입력받기 위해 Scanner 클래스를 이용해 scn 오브젝트 생성
 		int Champion = scn.nextInt(); //nextInt메소드를 사용해 입력 받음
 		
 		if (Champion == 1) {
